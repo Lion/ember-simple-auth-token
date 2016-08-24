a9a548512d7eef5d8c35940f9de9aaea97c3242c
Kyle Nathan <kyle@wearelion.com>
2016-08-24 16:58:55 +0100

    Enable babel loose modules flag for IE8

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-simple-auth-token'
+  name: 'ember-simple-auth-token',
+
+  init: function() {
+    this._super && this._super.init && this._super.init.apply(this, arguments);
+
+    this.options = this.options || {};
+    this.options.babel = this.options.babel || {};
+    this.options.babel.loose = true;
+  }
 };
